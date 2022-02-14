0510ac50cc97bdb3be986c5cbb82fed7036d6bfe
Tim Wuyts <me@timwuyts.be>
2022-02-14 14:53:04 +0100

    simplified description cleaning, still allowing raw data

--- app/Classes/CleanDescription.php
+++ app/Classes/CleanDescription.php
@@ -6,22 +6,9 @@ use App\Classes\Conversion;
 
 class CleanDescription extends Conversion {
     protected function convert() {
-        $parts = array_filter(explode(' ', strtolower($this->originalValue)));
+        $replaceMultipleSpaces = preg_replace('/ +/', ' ', $this->originalValue);
+        $replaceSemicolonSpaces = preg_replace('/ :/', ':', $replaceMultipleSpaces);
 
-        if (!is_array($parts) || count($parts) === 0) {
-            return;
-        }
-
-        if ($parts[0] === 'nummer') {
-            $subset = array_slice($parts, 5, -7);
-        }
-
-        if (array_search('bic', $parts)) {
-            $subset = array_slice($parts, 5, -12);
-        }
-
-        if (isset($subset)) {
-            $this->convertedValue = implode(' ', $subset);
-        }
+        $this->convertedValue = $replaceSemicolonSpaces;
     }
 }
